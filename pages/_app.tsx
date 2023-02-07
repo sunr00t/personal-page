39aa7b3c8c73258aa7576d9184ce491a9bda869c
Bruno Nascimento <sunr00t@gmail.com>
2023-02-07 00:43:01 -0300

    basic template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Navigation from '../components/Navigation';
+import { ThemeProvider } from 'next-themes'
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider attribute="class" enableSystem={false}>
+      <Navigation />
+      <Component {...pageProps} />
+      <Footer />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
