7e73f4dacc23f7a2da046e1506014e5f08d74018
Bruno Nascimento <sunr00t@gmail.com>
2023-03-19 09:39:10 -0300

    update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
         <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
-          <p>Built with Next.js, Tailwind</p>
+          <p>Built with ❤️ Next.js and Tailwind</p>
           <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
             <a
               href="https://twitter.com/sunr00t"
@@ -34,12 +34,12 @@ const Footer = () => {
             </a>
 
             <a
-              href="https://brunonascimento.dev.br"
+              href="https://wwww.brunonascimento.dev.br"
               className={'transition-colors hover:text-yellow-500'}
               target="_blank"
               rel="noreferrer"
             >
-              DEV
+              Personal Page
             </a>
           </div>
         </div>
