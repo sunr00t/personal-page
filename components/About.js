077ac32188774ac0caf4f2b6271de4b8e4da2671
Bruno Nascimento <sunr00t@gmail.com>
2024-03-13 10:37:07 -0300

    update profile image

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import profile from '../public/profile.png';
+import profile from '../public/profile2.jpeg';
 
 const About = () => {
   return (
