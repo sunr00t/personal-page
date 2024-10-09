b2e21a8f57196443136fc382180b4c625394bc73
Bruno Nascimento <sunr00t@gmail.com>
2024-10-09 20:36:02 -0300

    add ms-clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import Navigation from '../components/Navigation';
 import { ThemeProvider } from 'next-themes'
 import Footer from '../components/Footer';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,15 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <Footer />
       </ThemeProvider>
+      <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
     </>
   );
 }
