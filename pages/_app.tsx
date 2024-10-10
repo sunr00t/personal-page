00c6343ff0012afcc2b978e67ce4d33ca6049720
Bruno Nascimento <sunr00t@gmail.com>
2024-10-10 11:52:27 -0300

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,18 @@ import type { AppProps } from 'next/app';
 import Navigation from '../components/Navigation';
 import { ThemeProvider } from 'next-themes';
 import Footer from '../components/Footer';
-
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Script id="clarity-script" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "ofw9kaxip7");`}
+      </Script>
       <ThemeProvider attribute="class" enableSystem={false}>
         <Navigation />
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React  from "react";
 import Head from 'next/head'
 import About from "../components/About";
-import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -10,13 +9,6 @@ export default function Home() {
       <Head>
         <title>Bruno Nascimento - Developer</title>
         <link rel="icon" href="/favicon.ico" />
-        <Script id="clarity-script" strategy="afterInteractive">
-        {`(function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "ofw9kaxip7");`}
-      </Script>
       </Head>
 
       <main className="max-w-4xl mx-auto mt-16 antialiased">
